bab3af0b5680485aea8a4df08432e606749e3e94
mukhlisrambe5 <71597397+mukhlisrambe5@users.noreply.github.com>
2024-05-09 16:02:58 +0700

    video 42 lanjutan

---
+++ src/Components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components';
+import bg from '../img/bg.svg' 
+function Header() {
+  return (
+    <HeaderStyled>
+        <div className='header-content'>Hello World</div>
+    </HeaderStyled>
+  )
+}
+const HeaderStyled = styled.header`
+height: 100vh;
+width: 100%;
+background-image: url(${bg});
+background-repeat: no-repeat;
+background-size: cover;
+background-position-y: 100%;
+.header-content{
+    padding: 0 10rem;
+}
+`;
+export default Header 
---
+++ src/styles/Layout.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const OuterLayout = styled.section `
+    padding: 5rem 10rem;
+    background-position: 100%;
+`;
+
+export const InnerLayout = styled.section ``;       
